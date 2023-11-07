853609579a795c342c3c7627117f26b42c0d227a
chandiosamad07 <absamadchandio@gmail.com>
2023-11-07 13:39:08 +0500

    Conditional

---
+++ conditional.js
@@ -0,0 +1,22 @@
+// let age = 10;
+
+// if(age >= 18){
+//     console.log('you are adult ');
+// }
+// else{
+//     console.log('you are not adult');
+// }
+
+let day=2
+switch(day){
+
+    case 1:
+        console.log('Monday');
+        break
+    case 2:
+        console.log('Tuesday')
+        break
+    default:
+        console.log('invilad')
+        break
+}
