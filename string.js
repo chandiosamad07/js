5dd8c68c592da9e81ad86b01ba36824b41ce6cc6
chandiosamad07 <absamadchandio@gmail.com>
2023-11-07 13:33:04 +0500

    Slice and substring method

--- string.js
+++ string.js
@@ -5,3 +5,9 @@ console.log(typeof(str1), typeof(str2));
 
 console.log(str1[6]);
 console.log(str2[7]);
+
+
+let str3 = 'Hello, World'
+
+console.log(str3.slice(7,12));
+console.log(str3.substring(7,12));
