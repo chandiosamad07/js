210864e779b321e6de73c80957b5a4afa553ecca
chandiosamad07 <absamadchandio@gmail.com>
2023-11-07 13:09:06 +0500

    Operators

---
+++ operators.js
@@ -0,0 +1,37 @@
+// let x= 10;
+// let y=5;
+
+// let sum = x+y;
+
+// let differnce= x-y;
+
+// let  product = x * y;
+
+// let quotient = x / y;
+
+
+// let remainder = x % y;
+
+
+// let result = x ** y;
+
+
+// console.log("sum of X + y = " , sum);
+// console.log("Differnce of X - y = " , differnce);
+// console.log("product of X * y = " , product);
+// console.log("quotient of X / y = " , quotient);
+// console.log("remainder of X % y = " , remainder);
+// console.log("Result  of X ** y = " , result);
+
+
+let a =10;
+let b =5;
+
+console.log(a == b);
+console.log(a!= b);
+console.log(a === b);
+console.log(a !== b);
+console.log(a > b);
+console.log(a < b);
+console.log(a >= b);
+console.log(a <= b);
