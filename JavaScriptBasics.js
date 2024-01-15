31842b33b0c79cedf251b2bc836283c7747da540
chandiosamad07 <absamadchandio@gmail.com>
2024-01-15 09:29:42 +0500

    Veriables in JavaScript

--- JavaScriptBasics.js
+++ JavaScriptBasics.js
@@ -6,8 +6,20 @@ multiple Line comment */
 
 // single line Comment
 
-console.log("Single line comment");
+// console.log("Single line comment");
 
 /*multiple line comment */
 
-console.log("Multiple line comment");
+// console.log("Multiple line comment");
+
+// JavaScript Variable
+var x =10;
+var y = "A";
+var y = "B";
+const check = 5;
+let a = 5;
+a = "samad";
+console.log(check);
+console.log(a);
+console.log(x);
+console.log(y);
