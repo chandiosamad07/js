a32ca062d4df4c72e9975534381422f0fb04b454
chandiosamad07 <absamadchandio@gmail.com>
2024-01-15 09:16:29 +0500

    Comments in JavaScript

---
+++ JavaScriptBasics.js
@@ -0,0 +1,13 @@
+//Comment in JavaScript
+
+/*Two types of comment
+singal line Comment
+multiple Line comment */
+
+// single line Comment
+
+console.log("Single line comment");
+
+/*multiple line comment */
+
+console.log("Multiple line comment");
