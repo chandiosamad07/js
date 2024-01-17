d934714e178c6eff2bb9fc419ffe8dcf390be682
chandiosamad07 <absamadchandio@gmail.com>
2024-01-17 10:26:32 +0500

    Es6 Default parameter

--- Es6.js
+++ Es6.js
@@ -50,9 +50,16 @@
 
 //Map
 
-const fruits = new Map([
-    ["apples", 500],
-["bananas", 300],
-["oranges", 200]
-])
-console.log(fruits);
+// const fruits = new Map([
+//     ["apples", 500],
+// ["bananas", 300],
+// ["oranges", 200]
+// ])
+// console.log(fruits);
+
+//default paramter
+
+// function myfunction(x,y=10){
+//     return x + y
+// }
+// console.log(myfunction(5));
