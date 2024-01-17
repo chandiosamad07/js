b340a94d3f75ad32f0f53dea34026a7f85ec3217
chandiosamad07 <absamadchandio@gmail.com>
2024-01-17 10:09:51 +0500

    Es6 Speard (...) operator

--- Es6.js
+++ Es6.js
@@ -17,3 +17,16 @@
 
 // const z = (x,y)=> {return x*y};
 // console.log(z(2,3));
+
+//The Spread (...) operator
+
+const q1 = ["Jan", "Feb", "Mar"];
+const q2 = ["Apr", "May", "Jun"];
+const q3 = ["Jul", "Aug", "Sep"];
+const q4 = ["Oct", "Nov", "May"];
+const year = [...q1,...q2,...q3,...q4]
+console.log(year)
+
+const number = [23,55,21,87,56]
+let maxValue = Math.max(...number);
+console.log(maxValue)
