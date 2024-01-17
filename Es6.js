a0e8934fb8a10f008c041fd4d1c37b4f2079aabc
chandiosamad07 <absamadchandio@gmail.com>
2024-01-17 10:06:46 +0500

    Es6 Arrow Funcation

--- Es6.js
+++ Es6.js
@@ -1,9 +1,19 @@
 //JavaScript let
 
-var x = 10;
-{
-    let x = 2;
-    console.log(x)
-}
+// var x = 10;
+// {
+//     let x = 2;
+//     console.log(x)
+// }
 
-console.log(x)
+// console.log(x)
+
+//Arrow Funcation
+
+// var x = function(x,y){
+//     return x*y
+// }
+// console.log(x(2,2));
+
+// const z = (x,y)=> {return x*y};
+// console.log(z(2,3));
