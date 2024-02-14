a7dbb79f1ce6152a38077dacb5d4bd5463206c8d
chandiosamad07 <absamadchandio@gmail.com>
2024-02-14 23:03:07 +0500

    variable

--- string.js
+++ string.js
@@ -1,5 +1,5 @@
 let str1 = 'Abdul Samad chandio';
-let str2 = `Abdul samad Chandio`;
+let str2 = ` Chandio`;
 
 console.log(typeof(str1), typeof(str2));
 
@@ -7,7 +7,7 @@ console.log(str1[6]);
 console.log(str2[7]);
 
 
-let str3 = 'Hello,  chandio'
+let str3 = 'Hello, Abdul Samad chandio'
 
 console.log(str3.slice(7,12));
 console.log(str3.substring(7,12));
