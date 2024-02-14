c1103b2752c691879ebbbb4306d638f357817853
chandiosamad07 <absamadchandio@gmail.com>
2024-02-14 23:02:37 +0500

    variable

--- string.js
+++ string.js
@@ -7,7 +7,7 @@ console.log(str1[6]);
 console.log(str2[7]);
 
 
-let str3 = 'Hello, Abdul Samad chandio'
+let str3 = 'Hello,  chandio'
 
 console.log(str3.slice(7,12));
 console.log(str3.substring(7,12));
