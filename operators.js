87379c94764f628366c181cc4353fa0c64d18386
chandiosamad07 <absamadchandio@gmail.com>
2024-03-07 16:58:31 +0500

    oprators output

--- operators.js
+++ operators.js
@@ -27,11 +27,3 @@
 let a =18;
 let b =15;
 
-console.log(a == b);
-console.log(a!= b);
-console.log(a === b);
-console.log(a !== b);
-console.log(a > b);
-console.log(a < b);
-console.log(a >= b);
-console.log(a <= b);
