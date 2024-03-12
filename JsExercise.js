c7191828c7ccb1d34e78d4da779b0d18672659b0
chandiosamad07 <absamadchandio@gmail.com>
2024-03-12 14:56:15 +0500

    js

--- JsExercise.js
+++ JsExercise.js
@@ -139,18 +139,7 @@ Write a JavaScript program to find the area of a triangle where three sides are 
 //   console.log("Hello Abdul This is Arrow Funcation")
 // }
 
-// let hello = () => "Hello samad"
-// console.log(hello())
-
-// let hello = (...arg) =>{
-//   let sum = 0;
-//   for(let value of arg){
-//     sum = sum+value
-//   }
-//   console.log(sum)
-// }
-
-// hello(1,2,3,4,5,6,7,8,9,0,-10,11)
+)
 
 
 const people = [
