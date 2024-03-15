a8fb1a85e59b876b721abe3ac7f80dfdc92ee809
chandiosamad07 <absamadchandio@gmail.com>
2024-03-15 22:07:24 +0500

    condition

--- conditional.js
+++ conditional.js
@@ -7,23 +7,6 @@
 //     console.log('you are not adult');
 // }
 
-// let day=2
-// switch(day){
-
-//     case 1:
-//         console.log('Monday');
-//         break
-//     case 2:
-//         console.log('Tuesday')
-//         break
-//     default:
-//         console.log('invilad')
-//         break
-// }
-
-// const age = 26;
-// const check  = age >= 21 ? "Samad" : "Abdul";
-// console.log(check);
 
 const name = 'Abdul';
 const check = name =="Abdul" ? "Abdul" : "samad";
