c9a54fa722ecd0b83e2dc8c7722c3fa23b4d715e
chandiosamad07 <absamadchandio@gmail.com>
2024-06-28 09:08:50 +0500

    update js

--- string.js
+++ string.js
@@ -6,3 +6,8 @@ console.log(typeof(str1), typeof(str2));
 console.log(str1[6]);
 console.log(str2[7]);
 
+
+let str3 = 'Hello, Abdul Samad chandio'
+
+console.log(str3.slice(7,12));
+console.log(str3.substring(7,12));
