91e504b5daa9f6eab2b82e7e12d6010a40cd5dac
chandiosamad07 <absamadchandio@gmail.com>
2024-08-03 10:40:57 -0700

    High order function

--- HOF.js
+++ HOF.js
@@ -2,5 +2,8 @@ function Greet(name) {
     return `Hi ${name}`
 }
 
+function Great_name(greeting ,message,name){
+    console.log(`${greeting(name)},${message}`)
+}
 
 Great_name(Greet,'wel come to MERN Stack course','Samad')
