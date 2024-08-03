96dbc0334867122065a822423bcc6593f59c689e
chandiosamad07 <absamadchandio@gmail.com>
2024-08-03 10:22:39 -0700

    CallBack Example 2

--- Callback.js
+++ Callback.js
@@ -1,11 +1,26 @@
-
+//CallBack Example 1
 const Goodby=()=>{
     console.log('Good By')
 }
-
 function Message(name ,callback){
     console.log(`Hello ${name}`)
     callback()
 }
-
 Message("Abdul",Goodby)
+//CallBack Example 2
+
+function calculate(a,b,callback){
+    console.log(`Calculating with ${a} and ${b}`);
+    const result = callback(a,b)
+    console.log(`Result : ${result}`)
+}
+
+function add(x, y) {
+    return x + y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+calculate(12,14,add)
